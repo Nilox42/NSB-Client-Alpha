5b5e140735efb68c98c4b991b97e58795ee1bfb2
Nilox42 <jtmauerman@gmail.com>
2022-04-02 18:53:39 +0200

    a.0.0.0.3

---
+++ Install .Net Runtime .bat
@@ -0,0 +1,4 @@
+powershell -Command "Invoke-WebRequest https://download.visualstudio.microsoft.com/download/pr/7f3a766e-9516-4579-aaf2-2b150caa465c/d57665f880cdcce816b278a944092965/windowsdesktop-runtime-6.0.3-win-x64.exe -OutFile windowsdesktop-runtime-6.0.3-win-x64.exe"
+start "/../" windowsdesktop-runtime-6.0.3-win-x64.exe
+
+pause
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NSB Client Alpha
  
+# if you get errors while starting the Client run the Install .Net Runtime.bat file to install the .Net Runtime.
+# Then restart your PC and run the Client again.
